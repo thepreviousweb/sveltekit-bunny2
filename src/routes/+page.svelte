70d5257bed26b579038f263df7e91843a11a66bf
ThePreviousWeb <mail@thepreviousweb.com>
2026-08-16 03:16:57 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <h1>Home</h1>
 <p>
-	Simpele SvelteKit demo voor Bunny.net Edge Scripting via
+	Simple SvelteKit demo for Bunny.net Edge Scripting via
 	<code>sveltekit-adapter-bunny</code>.
 </p>
-<p>Ga naar <a href="/about">About</a> of <a href="/contact">Contact</a>.</p>
+<p>Go to <a href="/about">About</a> of <a href="/contact">Contact</a>.</p>
